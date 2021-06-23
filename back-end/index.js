53e70326daeed1402aa95b07e2613fb611981ad8
Fernando Antunes <fernando.antunes1@gmail.com>
2021-06-23 15:21:37 -0300

    criado arquivo routes.js

--- back-end/index.js
+++ back-end/index.js
@@ -4,10 +4,6 @@ const router = require('./routes');
 
 app.use(router);
 
-app.get("/", (req, res) => {
-  res.send("Server no ar");
-});
-
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
