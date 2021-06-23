83d4b51b3d60229d0fa16da6ccdd6c94b91fdc51
Fernando Antunes <fernando.antunes1@gmail.com>
2021-06-23 10:32:05 -0300

    criado model tag

---
+++ back-end/models/tag.js
@@ -0,0 +1,12 @@
+const tagModel = (sequelize, DataTypes) => {
+  const Tag = sequelize.define(
+    "Tag",
+    {
+      name: DataTypes.STRING,
+    },
+  );
+
+  return Tag;
+};
+
+module.exports = tagModel;
