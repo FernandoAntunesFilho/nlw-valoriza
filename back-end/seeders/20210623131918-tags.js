e776349c41972513cb13f9c0e25411efe9dcb812
Fernando Antunes <fernando.antunes1@gmail.com>
2021-06-23 10:24:03 -0300

    criado seed para a tabela tags

---
+++ back-end/seeders/20210623131918-tags.js
@@ -0,0 +1,25 @@
+module.exports = {
+  up: async (queryInterface, _Sequelize) => {
+    await queryInterface.bulkInsert(
+      "Tags",
+      [
+        {
+          id: 1,
+          name: "Fenomenal",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: 2,
+          name: "Talentoso",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+    );
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.bulkDelete("Tags", null, {});
+  },
+};
