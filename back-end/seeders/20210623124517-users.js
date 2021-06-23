87ecc7da9caffdb2aa25c3704464366539619516
Fernando Antunes <fernando.antunes1@gmail.com>
2021-06-23 10:18:45 -0300

    criado seed para a tabela users

---
+++ back-end/seeders/20210623124517-users.js
@@ -0,0 +1,31 @@
+module.exports = {
+  up: async (queryInterface, _Sequelize) => {
+    await queryInterface.bulkInsert(
+      "Users",
+      [
+        {
+          id: 1,
+          name: "Super User",
+          email: "super.user@nlw.com",
+          password: "ferfilho1983",
+          admin: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: 2,
+          name: "Davi Antunes",
+          email: "davi.antunes@nlw.com",
+          password: "123456",
+          admin: false,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+    );
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.bulkDelete("Users", null, {});
+  },
+};
