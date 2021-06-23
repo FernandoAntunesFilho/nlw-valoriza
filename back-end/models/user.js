b9731d2f158ca22cf0de1f7bab6d47ef9b03c7c4
Fernando Antunes <fernando.antunes1@gmail.com>
2021-06-23 10:30:46 -0300

    criado model user

---
+++ back-end/models/user.js
@@ -0,0 +1,15 @@
+const userModel = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    "User",
+    {
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      admin: DataTypes.BOOLEAN,
+    },
+  );
+
+  return User;
+};
+
+module.exports = userModel;
