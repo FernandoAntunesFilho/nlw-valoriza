921fb4df759793bb9b82fcdc3c8622ca76ec5fe8
Fernando Antunes <fernando.antunes1@gmail.com>
2021-06-24 10:31:37 -0300

    criado migration de criaçao da tabela compliments

---
+++ back-end/migrations/20210624131817-create-table-compliments.js
@@ -0,0 +1,54 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("Compliments", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      userSender: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Users",
+          key: 'id',
+        },
+      },
+      userReceiver: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Users",
+          key: 'id',
+        },
+      },
+      tagId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Tags",
+          key: 'id',
+        },
+      },
+      message: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("Compliments");
+  },
+};
