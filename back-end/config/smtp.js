3649efcb8839f0b9f5a6fcfc548ed482ee3b6f74
Fernando Antunes <fernando.antunes1@gmail.com>
2021-07-08 23:07:23 -0300

    Configurado email para uso do mailtrap

--- back-end/config/smtp.js
+++ back-end/config/smtp.js
@@ -3,6 +3,6 @@ require("dotenv");
 module.exports = {
   host: process.env.HOST,
   port: process.env.GMAIL_PORT,
-  user: process.env.USER,
+  user: process.env.MAIL_USER,
   pass: process.env.PASS,
 };
--- back-end/services/complimentSendEmailService.js
+++ back-end/services/complimentSendEmailService.js
@@ -1,22 +1,31 @@
 const nodemailer = require("nodemailer");
 const smtpConfig = require("../config/smtp");
 
-const transporter = nodemailer.createTransport({
-  // host: smtpConfig.host,
-  // port: smtpConfig.port,
-  service: "gmail",
-  secure: false,
+var transport = nodemailer.createTransport({
+  host: smtpConfig.host,
+  port: smtpConfig.port,
   auth: {
     user: smtpConfig.user,
     pass: smtpConfig.pass,
-  },
-  tls: {
-    rejectUnauthorized: false,
-  },
+  }
 });
 
+// const transporter = nodemailer.createTransport({
+//   host: smtpConfig.host,
+//   port: smtpConfig.port,
+//   // service: "gmail",
+//   secure: false,
+//   auth: {
+//     user: smtpConfig.user,
+//     pass: smtpConfig.pass,
+//   },
+//   // tls: {
+//   //   rejectUnauthorized: false,
+//   // },
+// });
+
 module.exports = async () => {
-  const mailSent = await transporter.sendMail({
+  const mailSent = await transport.sendMail({
     text: "Texto da mensagem",
     subject: "Você recebeu um elogio",
     from: "Fernando Antunes <fernando.antunes1@gmail.com>",
